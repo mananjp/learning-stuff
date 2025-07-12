74de65bdd81044ea2262cd95b04378a305782b72
Ansh <dhananiansh01@gmail.com>
2025-07-12 12:59:37 +0530

    commit..

--- stock/src/App.jsx
+++ stock/src/App.jsx
@@ -342,7 +342,7 @@ const CompanyProfitPredictor = () => {
   const colors = ['#3b82f6', '#ef4444', '#10b981', '#f59e0b', '#8b5cf6', '#06b6d4', '#f97316', '#84cc16', '#ec4899', '#6366f1'];
 
   return (
-    <div className="min-h-screen"> 
+    <div className="min-h-screen background-image: linear-gradient(to left top, #816bd1, #6e81df, #6195e6, #5ea7e9, #69b7e9, #59b6ef, #43b6f4, #20b5fa, #00a1ff, #008aff, #006dff, #6a41ff);"> 
       {/* Header */}
       <div className=" rounded-full bg-white shadow-lg border-b border-slate-200">
         <div className="max-w-7xl  mx-auto px-4 sm:px-6 lg:px-8">
@@ -369,12 +369,12 @@ const CompanyProfitPredictor = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         {/* Tab Navigation */}
         <div className="mb-8">
-          <div className="flex space-x-1 bg-white justify-evenly np-1 rounded-lg shadow-sm border border-slate-200">
+          <div className="flex space-x-1 p-2.5 bg-white justify-evenly np-1 rounded-lg shadow-sm border border-slate-200">
             <button
               onClick={() => setActiveTab('upload')}
               className={`px-4 py-2  rounded-md font-medium transition-all ${activeTab === 'upload'
                 ? 'bg-blue-600 text-white shadow-sm'
-                : 'text-slate-100 hover:text-slate-900 hover:bg-slate-50'
+                : 'text-slate-600 hover:text-white hover:bg-blue-700'
                 }`}
             >
               <Upload className="h-4 w-4 inline mr-2" />
@@ -384,7 +384,7 @@ const CompanyProfitPredictor = () => {
               onClick={() => setActiveTab('analysis')}
               className={`px-4 py-1 rounded-md font-medium transition-all ${activeTab === 'analysis'
                 ? 'bg-blue-600 text-white shadow-sm'
-                : 'text-slate-600 hover:text-slate-900 hover:bg-slate-50'
+                : 'text-slate-600 hover:text-white hover:bg-blue-600'
                 }`}
               disabled={data.length === 0}
             >
